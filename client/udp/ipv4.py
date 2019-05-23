d7ca59d7e2c120ed2c0cf7a199b16eccd1f017fe
tomocy <tomocy.dev@gmail.com>
2019-05-23 17:59:28 +0900

    add a client which requests in ipv4 using udp

---
+++ client/udp/ipv4.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9000
+BUFSIZE = 4096
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto(b"Hello", (HOST, PORT))
+
+resp = client.recv(BUFSIZE)
+print(resp.decode("UTF-8"))
+
+client.close()
